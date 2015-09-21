85a7917bd5abfd0a7b608a39ac56ef9f49e9fd3f
Chandramouli Soorian <cs@quantads.net>
2015-09-21 18:12:57 +0200

    Some partial additions

--- butter/src/cljs/butter/core.cljs
+++ butter/src/cljs/butter/core.cljs
@@ -2,9 +2,34 @@
   (:require [reagent.core :as reagent]
             [cljsjs.react]))
 
+(def login-info (reagent/atom {:username "root" :password ""}))
+
+(defn custom-input [param type value]
+    [:span
+        [:input {:type type
+                 :value value
+                 :on-change #(swap! login-info assoc param (.-target.value %))}]
+    ]
+    )
+
+(defn login-form []
+    [:div 
+        [custom-input :username "text" "root"]
+        [custom-input :password "password" ""]
+    ]
+    )
+
+(defn server-connection []
+    [:div
+        [:h1 "MySql server log in"]
+        [login-form]
+        ])
+
 (defn main-page
   []
-  [:div "Hello World!"])
+  [:div
+    [server-connection]]
+  )
 
 (defn mount-root
   []
